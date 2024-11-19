0e1a44327ff2bf5ddc5b64630e9c38f2a1a98cbd
sheen <sheen_vempeny@hotmail.com>
2024-11-19 11:49:46 -0600

    Initial Commit

---
+++ TestBrowserTests/TestBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestBrowserTests.swift
+//  TestBrowserTests
+//
+//  Created by Sheen on 11/19/24.
+//
+
+import Testing
+@testable import TestBrowser
+
+struct TestBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
